1f8cad76e549bd49d4bf4dda73ef4145d07cb9c2
Dragosh51 <dragosh.bindila@gmail.com>
2024-04-13 15:02:11 +0100

    Final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,6 @@ import { fetchUser } from './store/actions/authActions'
 import Login from "../src/pages/Log-in/login";
 import Register from "../src/pages/register/register";
 import Home from './components/home/Home';
-import Cart from './components/cart/Cart';
-import Checkout from './components/checkout/Checkout';
-import NavBar from './components/navBar/NavBar';
 import Landing from './pages/landingPage/landingPage'
 
 
@@ -31,10 +28,6 @@ function App() {
       <Route path="/Login" element={<Login />} />
       <Route path="/Register" element={<Register />} />
       <Route path="/Home" exact element={<Home />} />
-      <Route path="/Cart" element={<Cart />} />
-      {/* <PrivateRoute path='/Home' element={<Home />} />
-          <PrivateRoute path='/Cart' element={<Cart />} />
-          <PrivateRoute path='/Checkout' element={<Checkout />} /> */}
     </Routes>
 
 
--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from "redux";
-import productsReducers from "./productsReducers";
 import authReducers from "./authReducers";
-import cartReducer from "./cartReducer";
 import workoutsReducers from "./workoutsReducers"; 
 
 export default combineReducers({
-  products: productsReducers,
   auth: authReducers,
-  cart: cartReducer,
   workouts: workoutsReducers, 
 });
